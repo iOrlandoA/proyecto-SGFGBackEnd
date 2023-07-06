2d830b48138d178dd5aadf704a31a2c0e0147a96
alearal12 <alejandro.araya@ucrso.info>
2023-07-05 21:44:41 -0600

    reverted some changes

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -52,13 +52,12 @@ module Api
       def is_paid
         start_date = params[:start_date]
         end_date = params[:end_date]
-        area_id = params[:area_id].to_i
+        area_type = params[:area_type].to_i
       
-        query = Bill.includes(:area).where(date_created: start_date..end_date, area_id: area_id, full_paid: false)
+        query = Bill.includes(:area).where(date_created: start_date..end_date, areas: { area_type: area_type }, full_paid: false)
       
         render json: query
       end
-      
       
       
 
